795d166ef84a381c45d1bf5958b9e5ad2e1531de
Chukwuemerie Iheanachor <chukwuemerieiheanachor3@gmail.com>
2024-11-24 18:13:43 +0100

    Add files via upload

---
+++ codingal/ATM.py
@@ -0,0 +1,15 @@
+class Bankaccount:
+    def_init_(self, blan
+
+    def deposit(self, amount)
+     self,balance  +=   
+
+ce)lf distrupt meachanism
+    
+    self,balance       def withdraw(self amount)
+     self,balance(self-= amount)
+
+def get_balance(self)
+     return self.balance
+    
+
